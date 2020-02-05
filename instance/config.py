c82413c8e5df131c53a8f4d943da82f34ac49def
puffyboa <julian@sankergroup.org>
2020-02-04 20:05:19 -0600

    gitignore add instance directory

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,6 @@
 
 # Instance configuration
 
-SECRET_KEY = 'you-will-never-guess'
-
 SENTRY_DSN = None
 
-SOUNDCLOUD_CLIENT_ID = "YUKXoArFcqrlQn9tfNHvvyfnDISj04zk"
+SOUNDCLOUD_CLIENT_ID = "wJ3iwkqswthCXMGaBX9lJeIZAIshvKtV"
